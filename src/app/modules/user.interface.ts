c44a894e757a6f01b255ead038b17af8f078c1ed
sajid1545 <clashking1545@gmail.com>
2023-11-23 00:21:33 +0600

    interface create for User

---
+++ src/app/modules/user.interface.ts
@@ -0,0 +1,29 @@
+export interface IFullName {
+    firstName: string;
+    lastName: string;
+}
+
+export interface IAddress {
+    street: string;
+    city: string;
+    country: string;
+}
+
+export interface IOrders {
+    productName: string;
+    price: number;
+    quantity: number;
+}
+
+export interface IUser {
+    userId: number;
+    userName: string;
+    password: string;
+    fullName: IFullName;
+    age: number;
+    email: string;
+    isActive: boolean;
+    hobbies: string[];
+    address: IAddress;
+    orders: IOrders[];
+}
