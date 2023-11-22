1db7ed2bcecaf2054f66ab3def34a750776cde9f
sajid1545 <clashking1545@gmail.com>
2023-11-23 01:01:45 +0600

    routes is initialized

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
+import { userRoutes } from './app/modules/user.routes';
 const app: Application = express();
 
 // parser
 app.use(express.json());
 app.use(cors());
+
+// applications routes
+app.use('/api/users', userRoutes);
 
 app.get('/', (req: Request, res: Response) => {
     res.status(200).send('Welcome to Assignment 2');
